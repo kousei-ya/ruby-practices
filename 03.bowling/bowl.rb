4bc7c82469da871b364b9f57db169daf854a8044
yano <k.yano@genbasupport.com>
2024-08-19 10:07:58 +0900

    不要な行を削除しました。 変数のスペルミスを修正しました。 変数名を変更しました。 if文の不要な()を省略しました。 sumに書き換えられるところを書き換えました。

--- 03.bowling/bowl.rb
+++ 03.bowling/bowl.rb
@@ -8,25 +8,24 @@ shots = scores.flat_map do |s|
   s == 'X' ? [10, 0] : s.to_i
 end
 
-frames = []
 frames = shots.each_slice(2).map do |s|
   s[0] == 10 ? [s[0]] : s
 end
 
 point = frames.each_with_index.sum do |current_frame, i|
   next_frame = frames[i + 1]
-  next_next_fram = frames[i + 2]
+  after_next_frame = frames[i + 2]
   if i < 9
     if current_frame[0] == 10
-      if (next_frame[0] == 10) && !next_next_fram.nil?
-        10 + next_frame[0] + next_next_fram[0]
+      if next_frame[0] == 10 && !after_next_frame.nil?
+        10 + next_frame[0] + after_next_frame[0]
       else
-        10 + next_frame[0] + next_frame[1]
+        10 + next_frame.take(2).sum
       end
-    elsif (current_frame[0] + current_frame[1] == 10) && !next_frame.nil?
+    elsif current_frame.sum == 10 && !next_frame.nil?
       10 + next_frame[0]
     else
-      current_frame[0] + current_frame[1]
+      current_frame.sum
     end
   else
     current_frame.sum
