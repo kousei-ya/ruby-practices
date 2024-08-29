53da28d524fad360830ba684857ba59a41cf9918
yano <k.yano@genbasupport.com>
2024-08-29 10:36:24 +0900

    commit3 文字寄せの数値を定数にしました。 parts_filseをtranseposeして縦に並べられるようにしました。 each_with_index文をeach文にしました。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,19 +1,20 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+ARRANGE = 20
 
 def main
   files = Dir.glob('*')
   column = 3
   row = files.size.ceildiv(column)
-  parts_files = files.each_slice(row).to_a
+  parts_files = files.each_slice(row).to_a.transpose
   print_files(parts_files)
 end
 
 def print_files(parts_files)
 arrange = 20
-  parts_files.each_with_index do |row,i|
-    parts_files.each_with_index do |column,j|
-      print (parts_files[j][i]).to_s.ljust(arrange) unless parts_files[j].nil?
+  parts_files.each do |column|
+    column.each do |row|
+      print row.to_s.ljust(ARRANGE) 
     end
     puts
   end
