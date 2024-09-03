2f38890ec6e7b4e4593a2d2aaea0ec1cdcfc6861
kousei-ya <k.yano@genbasupport.com>
2024-09-03 09:39:50 +0900

    commit5 transposeの前に配列の要素の数を統一するようにしました

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -6,12 +6,20 @@ def main
   files = Dir.glob('*')
   column = 3
   row = files.size.ceildiv(column)
-  parts_files = files.each_slice(row).to_a.transpose
-  print_files(parts_files)
+  parts_files = files.each_slice(row).to_a
+
+  max_size = parts_files.max_by{|order| order.size}.size
+  arrange_array = parts_files.each do |order|
+    while order.size < max_size
+      order << nil
+    end
+  end
+  transpose_array = arrange_array.transpose  
+  print_files(transpose_array)
 end
 
-def print_files(parts_files)
-  parts_files.each do |column|
+def print_files(transpose_array)
+  transpose_array.each do |column|
     column.each do |row|
       print row.to_s.ljust(ARRANGE) 
     end
