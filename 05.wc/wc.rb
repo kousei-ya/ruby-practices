962de5b670c184dc8651a903b3c7d846546d827e
kousei-ya <k.yano@genbasupport.com>
2024-09-24 10:08:38 +0900

    オプションを判定する部分の処理の書き方を変更しました。

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -35,29 +35,29 @@ def pipe_branch(options, files)
 end
 
 def print_files(str, options)
-  print "#{str.lines.count}\s".rjust(WIDTH) if do_options_lines(options).is_a?(TrueClass)
-  print "#{str.split.size}\s".rjust(WIDTH) if do_options_words(options).is_a?(TrueClass)
-  print "#{str.size}\s".rjust(WIDTH) if do_options_bytes(options).is_a?(TrueClass)
+  print "#{str.lines.count}\s".rjust(WIDTH) if line_option_enabled?(options)
+  print "#{str.split.size}\s".rjust(WIDTH) if word_option_enabled?(options)
+  print "#{str.size}\s".rjust(WIDTH) if byte_option_enabled?(options)
 end
 
 def print_total(files, options)
-  print "#{files.sum { |line| File.read("./#{line}").lines.count }}\s".rjust(WIDTH) if do_options_lines(options).is_a?(TrueClass)
-  print "#{files.sum { |word| File.read("./#{word}").split.size }}\s".rjust(WIDTH) if do_options_words(options).is_a?(TrueClass)
-  print "#{files.sum { |byte| File.read("./#{byte}").size }}\s".rjust(WIDTH) if do_options_bytes(options).is_a?(TrueClass)
+  print "#{files.sum { |line| File.read("./#{line}").lines.count }}\s".rjust(WIDTH) if line_option_enabled?(options)
+  print "#{files.sum { |word| File.read("./#{word}").split.size }}\s".rjust(WIDTH) if word_option_enabled?(options)
+  print "#{files.sum { |byte| File.read("./#{byte}").size }}\s".rjust(WIDTH) if byte_option_enabled?(options)
   print 'total'
   puts
 end
 
-def do_options_lines(options)
-  true if options[:lines] || options.empty?
+def line_option_enabled?(options)
+  options[:lines] || options.empty?
 end
 
-def do_options_words(options)
-  true if options[:words] || options.empty?
+def word_option_enabled?(options)
+  options[:words] || options.empty?
 end
 
-def do_options_bytes(options)
-  true if options[:bytes] || options.empty?
+def byte_option_enabled?(options)
+  options[:bytes] || options.empty?
 end
 
 main
