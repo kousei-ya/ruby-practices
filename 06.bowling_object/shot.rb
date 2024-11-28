e43af320a474feac5a132349842251901a5482c4
kousei-ya <k.yano@genbasupport.com>
2024-11-28 11:38:34 +0900

    Shotクラスを作成

--- 06.bowling_object/shot.rb
+++ 06.bowling_object/shot.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 class Shot
-  
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def convert_score
+    @scores.flat_map do |score|
+      score == 'X' ? [10, 0] : score.to_i
+    end
+  end
 end
