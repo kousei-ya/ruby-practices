664f1d80684fde44b2e59f2fe17a848e5fb16a9e
kousei-ya <k.yano@genbasupport.com>
2024-11-28 11:38:59 +0900

    Gameクラスを作成

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -1,3 +1,47 @@
+# frozen_string_literal: true
+
+require_relative 'shot'
+require_relative 'frame'
+
 class Game
-  
+  attr_reader :converted_shots
+
+  def initialize(score)
+    @scores = score.split(',')
+  end
+
+  def execution
+    shots = Shot.new(@scores)
+    @converted_shots = shots.convert_score
+    frames = Frame.new(@converted_shots)
+    converted_frames = frames.convert_shot
+    #calculation(converted_frames) #テストの際に使用
+    puts "#{calculation(converted_frames)}" #テストの際はコメントアウト
+  end
+
+  def calculation(converted_frames)
+    converted_frames.each_with_index.sum do |current_frame, i|
+      next_frame = converted_frames[i + 1]
+      after_next_frame = converted_frames[i + 2]
+      if i < 9
+        if current_frame[0] == 10
+          10 + [*next_frame, *after_next_frame].take(2).sum
+        elsif current_frame.sum == 10
+          10 + next_frame[0]
+        else
+          current_frame.sum
+        end
+      else
+        current_frame.sum
+      end
+    end
+  end
 end
+
+if ARGV[0].nil? || ARGV[0].empty?
+  puts 'スコアを指定してください。'
+  exit
+end
+
+game = Game.new(ARGV[0])
+game.execution
