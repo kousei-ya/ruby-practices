ca3ccc6cb44cc877fe80ffa6922f40fcde87e93c
kousei-ya <k.yano@genbasupport.com>
2024-11-28 11:37:59 +0900

    Frameクラスを作成

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 class Frame
-  
+  def initialize(converted_shots)
+    @converted_shots = converted_shots
+  end
+
+  def convert_shot
+    @converted_shots.each_slice(2).map do |converted_shot|
+      converted_shot[0] == 10 ? [converted_shot[0]] : converted_shot
+    end
+  end
 end
