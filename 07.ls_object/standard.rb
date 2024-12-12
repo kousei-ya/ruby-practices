a4d8ce2501a470da0f1953307657cadeda0d59a1
kousei-ya <k.yano@genbasupport.com>
2024-12-12 17:07:04 +0900

    Standardクラスを作成

---
+++ 07.ls_object/standard.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+ARRANGE = 20
+
+class Standard
+  def initialize(files)
+    @files = files
+  end
+
+  def display_files
+    transpose_array = divide_files(@files)
+    transpose_array.each do |column|
+      column.each do |row|
+        print row.to_s.ljust(ARRANGE)
+      end
+      puts
+    end
+  end
+
+  def divide_files(files)
+    column = 3
+    row = files.size.ceildiv(column)
+    parts_files = files.each_slice(row).to_a
+
+    max_size = parts_files.max_by(&:size).size
+    arrange_array = parts_files.each do |order|
+      order << nil while order.size < max_size
+    end
+    arrange_array.transpose
+  end
+end
