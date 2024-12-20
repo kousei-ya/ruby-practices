08b4cf9e03228ff659ec78f9610c735767a00e84
kousei-ya <k.yano@genbasupport.com>
2024-12-20 09:34:20 +0900

    LsクラスとOptionsクラスを統合

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -2,7 +2,8 @@
 # frozen_string_literal: true
 
 require_relative 'argparser'
-require_relative 'options'
+require_relative 'standard'
+require_relative 'detail'
 
 class Ls
   def initialize
@@ -11,8 +12,16 @@ class Ls
   end
 
   def main
-    option = Options.new(@options)
-    option.separate_process
+    sarg = @options[:all] ? File::FNM_DOTMATCH : 0
+    @files = Dir.glob('*', sarg)
+    @files = @files.reverse if @options[:reverse]
+    if @options[:long].nil?
+      view_files = Standard.new(@files)
+      view_files.display_files
+    else
+      detail_files = Detail.new(@files)
+      detail_files.detail_files_print
+    end
   end
 end
 
