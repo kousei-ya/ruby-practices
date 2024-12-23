ebed2d939c0be199805885864c6a79927164de9d
kousei-ya <k.yano@genbasupport.com>
2024-12-23 10:25:55 +0900

    ArgParserクラスをLsクラスに統合

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require_relative 'argparser'
+require 'optparse'
 require_relative 'column_file_display'
 require_relative 'file_detail_display'
 
 class Ls
   def initialize
-    @opt = ArgumentParser.new
-    @options = @opt.parse!(ARGV)
+    @options = {}
+    opt = OptionParser.new
+    opt.on('-a', '--all') { |a| @options[:all] = a }
+    opt.on('-r', '--reverse') { |r| @options[:reverse] = r }
+    opt.on('-l') { |l| @options[:long] = l }
+    opt.parse!(ARGV)
   end
 
   def main
